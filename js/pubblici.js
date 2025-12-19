0c6eb18164ec091e1437471f05a2938fe107988f
s_vnrlcu08d01l500e <s_vnrlcu08d01l500e@ltsp-current.itis-emattei.local>
2025-12-19 12:44:46 +0100

    palle

---
+++ js/pubblici.js
@@ -0,0 +1,16 @@
+// Array con i percorsi delle immagini
+const immagini = [
+    "./img/spid.jpg",
+    "./img/fascicolo-sanitario-elettronico800x450.jpg",
+    "./img/cie-id-1.jpg"
+];
+
+// Selezione dell'elemento immagine con id "copertina"
+let indice = 0;  // Impostiamo l'indice iniziale a 0 (prima immagine)
+const transizioni = document.getElementById("img-transizioni");
+
+// Funzione per cambiare immagine ogni 2 secondi
+setInterval(() => {
+    indice = (indice + 1) % immagini.length;  // Incrementiamo l'indice e torniamo a 0 quando arriva alla fine
+    transizioni.src = immagini[indice];  // Cambiamo la sorgente dell'immagine
+}, 2500);  // 2000 millisecondi = 2 secondi
